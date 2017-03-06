6b9fcd0749b67e4f91b0f2a47bf9abc1607ee3b5
bc022699 <Ben.Compton@cerner.com>
2017-03-06 11:21:14 -0500

    Fix timing issue where preloader sometimes remains in the DOM after being opened and closed very quickly

--- src/state-kernels/modals/preloader-kernel.ts
+++ src/state-kernels/modals/preloader-kernel.ts
@@ -28,6 +28,16 @@ export class PreloaderKernel extends StateKernel<IPreloaderState> {
     private hidePreloader() {
         if (this.preloaderModal) {
             this.framework7.hidePreloader();
+
+            const tempPreloaderModal = this.preloaderModal;
+
+            //Sometimes, a timing issue causes the preloader to not get cleaned up, so we're making sure it always does.
+            setTimeout(() => {
+                if (tempPreloaderModal) {
+                    tempPreloaderModal.remove();
+                }
+            }, 100);
+
             this.preloaderModal = null;
         }
     }
