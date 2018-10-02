087958cfc2b8b03d49c89b49fe714210e0504dc4
Compton <ben.compton@cerner.com>
2018-10-02 14:24:16 -0700

    Make sure F7 links don't change the browser URL

--- src/framework7-redux-plugin.ts
+++ src/framework7-redux-plugin.ts
@@ -6,6 +6,17 @@ export const framework7ReduxPlugin = {
         init: function () {
           var app = this;
 
+          // Make sure links in Framework7 don't change the URL
+          app.on('click', (e) => {				
+            const clicked = app.$(e.target);
+            const clickedLink = clicked.closest('a');
+            const isLink = clickedLink.length > 0;				
+            
+            if (isLink) {
+              e.preventDefault();
+            }
+          });          
+
           if (app.params.stateKernel) {
             app.params.stateKernel.setFramework7(app);
           } else {
